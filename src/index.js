602c781843be0c1ed859ae112a71ac2f76cab616
John Gamboa <jayjeex@gmail.com>
2021-07-27 15:22:33 +0800

    added nav in index.js

--- src/index.js
+++ src/index.js
@@ -25,3 +25,58 @@ const setupHeadTag = (() => {
     headElement.insertBefore(newLinkMaterialIcon, titleElement);
     headElement.appendChild(newLinkFavicon);
 })();
+
+const createApp = (() => {
+    const bodyElement = document.querySelector('body');
+    const bodyScript = document.querySelector('body script');
+    const app = document.createElement('div');
+
+    app.setAttribute('class', 'app');
+    bodyElement.insertBefore(app, bodyScript);
+})();
+
+// below > function might not be needed
+const dom = (() => {
+    const app = document.querySelector('.app');
+
+    return { 
+        app
+    };
+})();
+
+const createAppNav = (() => {
+
+    const nav = document.createElement('nav');
+    nav.setAttribute('class', 'nav-comp');
+    nav.innerHTML = `
+        <div class = "nc-left">
+            <button id = "nav-btn-menu">
+                <span class="material-icons-outlined mid">menu</span>
+            </button>
+            <button id = "nav-btn-inbox">
+                <span class="material-icons-outlined mid">home</span>
+            </button>
+            <form>
+                <div class = "search-div">
+                    <span class="material-icons-outlined mid">search</span>
+                    <input type="search" aria-label="Search through all todos" 
+                        class = "search-input" placeholder="Search"
+                        id = "nav-input-search">
+                    <button type = "reset">
+                        <span class="material-icons-outlined mid">close</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class = "nc-right">
+            <button id = "nav-btn-quickAdd">
+                <span class="material-icons-outlined mid">add</span>
+            </button>
+            <button id = "nav-btn-notification">
+                <span class="material-icons-outlined mid">notifications</span>
+            </button>
+        </div>
+    `;
+    
+    dom.app.appendChild(nav);
+})();
